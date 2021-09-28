5f509033aef1d70fed4e6e25bad0ae35f0d23a99
Chandler <chandlerekins@gmail.com>
2021-09-28 01:10:14 -0400

    moved initialization to functions and added initialization for timer0

--- main.c
+++ main.c
@@ -9,38 +9,59 @@
 #include <avr/interrupt.h>
 #include <util/delay_basic.h>
 
-#define DEBOUNCE_DELAY 1000L*50
-
 ISR(INT0_vect) {
 	PINB |= 1<<PB0;
+}
+
+void init_external_interrupt() {
+	// Enable INT0 interrupt
+	GIMSK |= (1<<INT0); 
+	
+	// Trigger interrupt on falling edge of INT0
+	MCUCR |= 1<<ISC01;
+	MCUCR &= ~(1<<ISC00);
+}
+
+void init_timer0_interrupt() {
+	// Normal port operation
+	TCCR0A &= ~(1<<COM0A1) & ~(1<<COM0A0);
+
+	// CTC mode
+	TCCR0A &= ~(1<<WGM00);
+	TCCR0A |= (1<<WGM01);
+	TCCR0B &= ~(1<<WGM02);
+
+	// Set clock divider
+	TCCR0B |= (1<<CS00) | (1<<CS02);
+	TCCR0B &= ~(1<<CS01);
+
+	// Set output compare value
+	OCR0A = 0xFF;
+	
+	// Enable interrupt for OCR0A
+	TIMSK |= 1<<OCIE0A;
+}
+
+void init_pins() {
+
+	//Set PB0, PB1, PB2, PB4 to output mode
+	DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB3) | (1<<PB4);
+
+	//Set PB2 to input mode with no pullup resistor
+	DDRB &= ~(1<<PB2);
+	PORTB &= ~(1<<PB2);
 }
 
 
 int main(void)
 {
-	//Set Pin as Output
-	DDRB |= 1<<PB0;
+	init_pins();
+	init_external_interrupt();
 
-	//Set Pin as INput pullup
-	DDRB &= ~(1<<PB2);
-	PORTB &= ~(1<<PB2);
-
-
-	//Set up external interrupt
-	GIMSK |= (1<<INT0);
-	
-	// falling edge of INT0
-	MCUCR |= 1<<ISC01;
-	MCUCR &= ~(1<<ISC00);
-
-
+	//Enable all interrupts
 	sei();
-	
 
-	while(1) {
-		
-
-	}
+	while(1);;
 
    	return 0;
 }
