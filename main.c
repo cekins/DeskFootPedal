a5888558ace2288034c4fd7432852b4b51cf300b
Chandler <chandlerekins@gmail.com>
2021-09-28 11:56:45 -0400

    changed output toggling method and added new initial pin state

--- main.c
+++ main.c
@@ -17,32 +17,30 @@ uint8_t time_count = 0;
 uint8_t press_count = 0;
 
 void press_button(uint8_t button) {
-	int pin;
 	switch (button) {
 		case 1:
-			pin = PB0;
+			PINB |= 1<<PB0;
 			break;
 		case 2:
-			pin = PB1;
+			PINB |= 1<<PB1;
 			break;
 		case 3:
-			pin = PB3;
+			PINB |= 1<<PB3;
 			break;
 		case 4:
-			pin = PB4;
+			PINB |= 1<<PB4;
 			break;
 		default:
 			return;
 	}
 	
-	PINB |= 1<<pin;
 }
 	
 ISR(TIMER0_COMPA_vect) {
 
 	if (time_count > PRESS_WINDOW)
 		return;
-	if (++time_count == PRESS_WINDOW && press_count > 0 && press_count <= 4) {
+	if (++time_count == PRESS_WINDOW) {
 		press_button(press_count);
 		press_count = 0;
 	}
@@ -84,8 +82,12 @@ void init_timer0_interrupt() {
 
 void init_pins() {
 
+	//Set PB0, PB1, PB2, PB4 to input pullup (to prevent intermediate low state)
+	PORTB |= (1<<PB0) | (1<<PB1) | (1<<PB3) | (1<<PB4);
+
 	//Set PB0, PB1, PB2, PB4 to output mode
 	DDRB |= (1<<PB0) | (1<<PB1) | (1<<PB3) | (1<<PB4);
+
 
 	//Set PB2 to input mode with no pullup resistor
 	DDRB &= ~(1<<PB2);
